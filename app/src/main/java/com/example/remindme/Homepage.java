55f96de6ab3ccc968494402cf9e255d94fad24ad
Julina Kay Gacura <jgacura.k11937320@umak.edu.ph>
2023-11-20 18:56:07 +0800

    Homepage-Update: Header done, Design and Code for cards or reminder list

--- app/src/main/java/com/example/remindme/Homepage.java
+++ app/src/main/java/com/example/remindme/Homepage.java
@@ -15,7 +15,7 @@ import java.util.Calendar;
 
 public class Homepage extends AppCompatActivity {
 
-    TextView greetingText, userNameText, titleText, descriptionText;
+    TextView greetingText, userNameText;
     private ImageView timeOfDayImage;
     private List<ReminderItem> reminderList;
 
@@ -33,18 +33,15 @@ public class Homepage extends AppCompatActivity {
         // Assuming you have an ImageView with the id "timeOfDayImage" in your layout
         timeOfDayImage = findViewById(R.id.timeOfDayImage);
 
-        titleText = findViewById(R.id.titleText);
-        descriptionText = findViewById(R.id.descriptionText);
-
         // Get the current time
         Calendar calendar = Calendar.getInstance();
         int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
 
         // Get the user name from the input screen (assuming it's passed through Intent)
-        String confirmUser = getIntent().getStringExtra("confirmUser");
+        String userName = getIntent().getStringExtra("userName");
 
         // Set the user name in the TextView
-        userNameText.setText("Hello, " + confirmUser + "!");
+        userNameText.setText("Hello, " + userName + "!");
 
         // Set the greeting and image based on the time of day
         if (hourOfDay >= 6 && hourOfDay < 12) {
@@ -60,34 +57,25 @@ public class Homepage extends AppCompatActivity {
     }
     // ReminderItem class with due date
     private static class ReminderItem {
-        private String titleText;
-        private String descriptionText;
-        private String dueDateText;
+        private String reminderTitle;
+        private String reminderDescription;
+        private String reminderTime;
 
-        ReminderItem(String titleText, String descriptionText, String dueDateText) {
-            this.titleText = titleText;
-            this.descriptionText = descriptionText;
-            this.dueDateText = dueDateText;
+        ReminderItem(String reminderTitle, String reminderDescription, String reminderTime) {
+            this.reminderTitle = reminderTitle;
+            this.reminderDescription = reminderDescription;
+            this.reminderTime = reminderTime;
         }
 
         // Getter methods for title, description, and due date
     }
 
-    // Update getDummyData to provide due dates
-    private List<ReminderItem> getDummyData() {
-        List<ReminderItem> reminderList = new ArrayList<>();
-        reminderList.add(new ReminderItem("Meeting", "Team meeting at 10 AM", "2023-11-20"));
-        reminderList.add(new ReminderItem("Call", "Call mom at 3 PM", "2023-11-21"));
-        // Add more dummy data as needed
-        return reminderList;
-    }
-
     // Update onBindViewHolder in ReminderAdapter to handle actions
     public void onBindViewHolder(ReminderViewHolder holder, int position) {
         ReminderItem item = reminderList.get(position);
-        holder.titleText.setText(item.titleText);
-        holder.descriptionText.setText(item.descriptionText);
-        holder.dueDateText.setText("Due Date: " + item.dueDateText);
+        holder.reminderTitle.setText(item.reminderTitle);
+        holder.reminderDescription.setText(item.reminderDescription);
+        holder.reminderTime.setText(item.reminderTime);
 
         // Set click listener for the expand icon
         holder.expandIcon.setOnClickListener(v -> {
@@ -98,16 +86,16 @@ public class Homepage extends AppCompatActivity {
 
     // Inner class for ViewHolder
     class ReminderViewHolder extends RecyclerView.ViewHolder {
-        TextView titleText;
-        TextView descriptionText;
-        TextView dueDateText;
+        TextView reminderTitle;
+        TextView reminderDescription;
+        TextView reminderTime;
         ImageView expandIcon;
 
         ReminderViewHolder(View itemView) {
             super(itemView);
-            titleText = itemView.findViewById(R.id.titleText);
-            descriptionText = itemView.findViewById(R.id.descriptionText);
-            dueDateText = itemView.findViewById(R.id.dueDateText);
+            reminderTitle = itemView.findViewById(R.id.reminderTitle);
+            reminderDescription = itemView.findViewById(R.id.reminderDescription);
+            reminderTime = itemView.findViewById(R.id.reminderTime);
             expandIcon = itemView.findViewById(R.id.expandIcon);
         }
     }
